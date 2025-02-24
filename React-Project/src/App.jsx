5f259a95e155dcb694bc5d74ee9fb07d9e124d9c
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-02-24 22:24:09 +0530

    Added few more files

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 function App() {
-  return (<h1>Hello World | Kratik Paliwal</h1>);
+  return (<h1>Hello World | Kratik Paliwal.....</h1>);
 }
 
 export default App
