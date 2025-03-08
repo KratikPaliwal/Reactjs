9fcd7a2c3fe4b404484e37b1c21289c07cebae58
Kratik Paliwal <kratikpaliwal19@gmail.com>
2025-03-08 22:41:25 +0530

    Added 05react_router

---
+++ 05react_router/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-blue-700 text-4xl'>React Router</div>
+    </>
+  )
+}
+
+export default App
